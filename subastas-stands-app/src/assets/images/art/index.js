b03dbedf480a24904c407c715d2761f320fec1db
Diego Flores <dfmonroy07@gmail.com>
2023-11-03 09:36:08 -0600

    upgraded code

--- subastas-stands-app/src/assets/images/art/index.js
+++ subastas-stands-app/src/assets/images/art/index.js
@@ -1,18 +1,20 @@
 import PlantlogicArt1 from './plantlogic-01.png';
 import PlantlogicArt2 from './plantlogic-02.png';
 
+const PLANTLOGIC_URL = "https://8th.io/be9wn"
+
 const PlantlogicArt = [
     {
         id: 1,
         name: 'El freserio',
         img: PlantlogicArt1,
-        link: 'https://8th.io/be9wn'
+        link: PLANTLOGIC_URL
     },
     {
         id: 2,
         name: 'Fresas duras de mata',
         img: PlantlogicArt2,
-        link: 'https://8th.io/be9wn'
+        link: PLANTLOGIC_URL
     },
 ]
 
