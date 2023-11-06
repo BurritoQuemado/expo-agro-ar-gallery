e709ccba96abe2f587a178e30f6b599dada45a7d
Diego Flores <dfmonroy07@gmai.com>
2023-11-06 11:37:31 -0600

    added arts and export them

--- subastas-stands-app/src/assets/images/art/index.js
+++ subastas-stands-app/src/assets/images/art/index.js
@@ -1,7 +1,53 @@
 import PlantlogicArt1 from './plantlogic-01.png';
 import PlantlogicArt2 from './plantlogic-02.png';
+import AdviceArt1 from './advice01.png';
+import AdviceArt2 from './advice02.png';
+import AdviceArt3 from './advice03.png';
+import BioteksaArt1 from './bioteksa01.png';
+import BioteksaArt2 from './bioteksa02.png';
+import BioteksaArt3 from './bioteksa03.png';
+import BufaloArt1 from './bufalo01.png';
+import BufaloArt2 from './bufalo02.png';
+import BufaloArt3 from './bufalo03.png';
+import CortevaArt1 from './corteva01.png';
+import CortevaArt2 from './corteva02.png';
+import CortevaArt3 from './corteva03.png';
+import IrritecManguerasArt1 from './irritecMangueras01.png';
+import IrritecManguerasArt2 from './irritecMangueras02.png';
+import IrritecManguerasArt3 from './irritecMangueras03.png';
+import IrritecRiegoArt1 from './irritecRiego01.png';
+import IrritecRiegoArt2 from './irritecRiego02.png';
+import IrritecRiegoArt3 from './irritecRiego03.png';
+import MSAgrosArt1 from './msagros01.png';
+import MSAgrosArt2 from './msagros02.png';
+import MSAgrosArt3 from './msagros03.png';
+import MSAgrosArt4 from './msagros04.png';
+import MSAgrosArt5 from './msagros05.png';
+import PierceArt1 from './pierce01.png';
+import RedSeaArt1 from './redSea01.png';
+import RedSeaArt2 from './redSea02.png';
+import RotoplasArt1 from './rotoplas01.png';
+import RotoplasArt2 from './rotoplas02.png';
+import SyngentaArt1 from './syngenta01.png';
+import SyngentaArt2 from './syngenta02.png';
 
 const PLANTLOGIC_URL = "https://8th.io/be9wn"
+const ADVICE_URL = ""
+const BIOTEKSA_URL = ""
+const BUFALO_URL = ""
+const CORTEVA_URL = ""
+const IRRITECMANGUERAS_URL = ""
+const IRRITECRIEGO_URL = ""
+const MSAGROS_URL = ""
+const PIERCE_URL = ""
+const REDSEA_URL = ""
+const ROTOPLAS_URL = ""
+const SYNGENTA_URL = ""
+const CLIMATEBOX_URL = ""
+const PRECISIONPLANTING_URL = ""
+const DESICO_URL = ""
+const AGRIBEST_URL = ""
+const NOUSBO_URL = ""
 
 const PlantlogicArt = [
     {
@@ -18,6 +64,230 @@ const PlantlogicArt = [
     },
 ]
 
+const AdviceArt = [
+    {
+        id: 1,
+        name: 'Advice1',
+        img: AdviceArt1,
+        link: ADVICE_URL
+    },
+    {
+        id: 2,
+        name: 'Advice2',
+        img: AdviceArt2,
+        link: ADVICE_URL
+    },
+    {
+        id: 3,
+        name: 'Advice3',
+        img: AdviceArt3,
+        link: ADVICE_URL
+    },
+]
+
+const BioteksaArt = [
+    {
+        id: 1,
+        name: 'Bioteksa1',
+        img: BioteksaArt1,
+        link: BIOTEKSA_URL
+    },
+    {
+        id: 2,
+        name: 'Bioteksa2',
+        img: BioteksaArt2,
+        link: BIOTEKSA_URL
+    },
+    {
+        id: 3,
+        name: 'Bioteksa3',
+        img: BioteksaArt3,
+        link: BIOTEKSA_URL
+    },
+]
+
+const BufaloArt = [
+    {
+        id: 1,
+        name: 'Bufalo1',
+        img: BufaloArt1,
+        link: BUFALO_URL
+    },
+    {
+        id: 2,
+        name: 'Bufalo2',
+        img: BufaloArt2,
+        link: BUFALO_URL
+    },
+    {
+        id: 3,
+        name: 'Bufalo3',
+        img: BufaloArt3,
+        link: BUFALO_URL
+    },
+]
+
+const CortevaArt = [
+    {
+        id: 1,
+        name: 'Corteva1',
+        img: CortevaArt1,
+        link: CORTEVA_URL
+    },
+    {
+        id: 2,
+        name: 'Corteva2',
+        img: CortevaArt2,
+        link: CORTEVA_URL
+    },
+    {
+        id: 3,
+        name: 'Corteva3',
+        img: CortevaArt3,
+        link: CORTEVA_URL
+    },
+]
+
+const IrritecManguerasArt = [
+    {
+        id: 1,
+        name: 'IrritecMangueras1',
+        img: IrritecManguerasArt1,
+        link: IRRITECMANGUERAS_URL
+    },
+    {
+        id: 2,
+        name: 'IrritecMangueras2',
+        img: IrritecManguerasArt2,
+        link: IRRITECMANGUERAS_URL
+    },
+    {
+        id: 3,
+        name: 'IrritecMangueras3',
+        img: IrritecManguerasArt3,
+        link: IRRITECMANGUERAS_URL
+    },
+]
+
+const IrritecRiegoArt = [
+    {
+        id: 1,
+        name: 'IrritecRiego1',
+        img: IrritecRiegoArt1,
+        link: IRRITECRIEGO_URL
+    },
+    {
+        id: 2,
+        name: 'IrritecRiego2',
+        img: IrritecRiegoArt2,
+        link: IRRITECRIEGO_URL
+    },
+    {
+        id: 3,
+        name: 'IrritecRiego3',
+        img: IrritecRiegoArt3,
+        link: IRRITECRIEGO_URL
+    },
+]
+
+const MSAgrosArt = [
+    {
+        id: 1,
+        name: 'MSAgros1',
+        img: MSAgrosArt1,
+        link: MSAGROS_URL
+    },
+    {
+        id: 2,
+        name: 'MSAgros2',
+        img: MSAgrosArt2,
+        link: MSAGROS_URL
+    },
+    {
+        id: 3,
+        name: 'MSAgros3',
+        img: MSAgrosArt3,
+        link: MSAGROS_URL
+    },
+    {
+        id: 4,
+        name: 'MSAgros4',
+        img: MSAgrosArt4,
+        link: MSAGROS_URL
+    },
+    {
+        id: 5,
+        name: 'MSAgros5',
+        img: MSAgrosArt5,
+        link: MSAGROS_URL
+    },
+]
+
+const PierceArt = [
+    {
+        id: 1,
+        name: 'Pierce1',
+        img: PierceArt1,
+        link: PIERCE_URL
+    },
+]
+
+const RedSeaArt = [
+    {
+        id: 1,
+        name: 'RedSea1',
+        img: RedSeaArt1,
+        link: REDSEA_URL
+    },
+    {
+        id: 2,
+        name: 'RedSea2',
+        img: RedSeaArt2,
+        link: REDSEA_URL
+    },
+]
+
+const RotoplasArt = [
+    {
+        id: 1,
+        name: 'Rotoplas1',
+        img: RotoplasArt1,
+        link: ROTOPLAS_URL
+    },
+    {
+        id: 2,
+        name: 'Rotoplas2',
+        img: RotoplasArt2,
+        link: ROTOPLAS_URL
+    },
+]
+
+const SyngentaArt = [
+    {
+        id: 1,
+        name: 'Syngenta1',
+        img: SyngentaArt1,
+        link: SYNGENTA_URL
+    },
+    {
+        id: 2,
+        name: 'Syngenta2',
+        img: SyngentaArt2,
+        link: SYNGENTA_URL
+    },
+]
+
 export {
-    PlantlogicArt
+    PlantlogicArt,
+    AdviceArt,
+    BioteksaArt,
+    BufaloArt,
+    CortevaArt,
+    IrritecManguerasArt,
+    IrritecRiegoArt,
+    MSAgrosArt,
+    PierceArt,
+    RedSeaArt,
+    RotoplasArt,
+    SyngentaArt
 }
