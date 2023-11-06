9886cda58197fc89e88fc6a64479ceeee8873194
Diego Flores <dfmonroy07@gmai.com>
2023-11-06 11:38:03 -0600

    feat: conditional rendering on art length

--- subastas-stands-app/src/components/art/art-display.jsx
+++ subastas-stands-app/src/components/art/art-display.jsx
@@ -7,28 +7,35 @@ function ExpositorArtList ({ arte }) {
 
     return (
         <>
-            <div className="px-6 py-4 sm:px-6 sm:py-32 lg:px-8">
-                <div className="mx-auto max-w-2xl text-center">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        Arte interactivo
-                    </h2>
-                    <div className="bg-white">
-                        <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                            <div className="grid grid-cols-2 gap-4 grid-flow-row  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                            {
-                                arte.map(obra =>{
-                                    return (
-                                        <ExpositorArtListItem key={obra.id} item={ obra } />
-                                    )
+            {
+                arte.length > 0 ? 
+                <>
+                <div className="px-6 py-4 sm:px-6 sm:py-32 lg:px-8">
+                    <div className="mx-auto max-w-2xl text-center">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                            Arte interactivo
+                        </h2>
+                        <div className="bg-white">
+                            <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                                <div className="grid grid-cols-2 gap-4 grid-flow-row  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                                {
+                                    arte.map(obra =>{
+                                        return (
+                                            <ExpositorArtListItem key={obra.id} item={ obra } />
+                                        )
 
+                                    } 
+                                    )
                                 } 
-                                )
-                            } 
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                </>
+                
+             : null
+            }
         </>
     )
 }
